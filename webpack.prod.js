34c7f9d264295e7e8c81cef474e146297e3e40ed
DanielAdek <maildaniel.me1@gmail.com>
2018-10-29 16:46:58 +0100

    chore(settings) Setup front-end application
    -Setup babel
    -Setup react
    -Setup eslint
    -Setup webpack
    
    [Finishes: #161545904]

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+          }
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: () => [
+              autoprefixer({
+                browsers: ['>1%', 'last 2 versions']
+              })
+            ]
+          }
+        },
+        {
+          loader: 'sass-loader'
+        }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+    })
+  ]
+});
